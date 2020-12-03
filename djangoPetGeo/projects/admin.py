439119dac98dffb839d528d05bd79dc2d3fc7f74
yashinimori <yashinimori@gmail.com>
2020-12-04 01:18:46 +0200

    Modifed admin page and created a few models

--- djangoPetGeo/projects/admin.py
+++ djangoPetGeo/projects/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import GeoPoint, Projects
 
-# Register your models here.
+
+class GeoPointAdmin(admin.ModelAdmin):
+    list_display = ('name', 'geo_data')
+
+
+admin.site.site_header = "Django Geo Pet"
+admin.site.register(GeoPoint, GeoPointAdmin)
--- djangoPetGeo/projects/models.py
+++ djangoPetGeo/projects/models.py
@@ -1,12 +1,12 @@
-from django.db import models
 from django.contrib.gis.db import models as geo_models
+from django.db import models
 
 
 class Projects(models.Model):
     pass
 
 
-class GeoObjects(models.Model):
+class GeoObjects(geo_models.Model):
     name = models.CharField(max_length=30)
     project = models.ForeignKey('Projects', on_delete=models.CASCADE)
 
@@ -18,4 +18,12 @@ class GeoObjects(models.Model):
 
 
 class GeoPoint(GeoObjects):
-    pass
+    geo_data = geo_models.PointField()
+
+
+class GeoLine(GeoObjects):
+    geo_data = geo_models.LineStringField()
+
+
+class GeoPoly(GeoObjects):
+    geo_data = geo_models.PolygonField()
